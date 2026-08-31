f66978cff0a7ef272d38978f719dfd4fcd4d41e2
Alexander Perez <alex@jaynconstruction.com>
2026-08-31 17:40:57 -0400

    Load shared dial scaffold styles

--- source/src/main.jsx
+++ source/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './layout-fixes.css'
 import './capacity.css'
+import './dial-scaffold.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
