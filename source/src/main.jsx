b274674463a0fb95cb7428e4bff8ab0810c3359f
Alexander Perez <alex@jaynconstruction.com>
2026-09-01 09:34:23 -0400

    Load Sep 1 interface review refinements

--- source/src/main.jsx
+++ source/src/main.jsx
@@ -5,7 +5,9 @@ import './layout-fixes.css'
 import './capacity.css'
 import './dial-scaffold.css'
 import './schedule-editor.css'
+import './restore-browser.css'
 import './recovery-ui.css'
+import './review-fixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
